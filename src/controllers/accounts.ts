53e94dbf1e8759009ba5936d017d58e0d86e5ef4
HowWhat42 <charlau30@gmail.com>
2022-12-08 09:48:15 +0100

    feat: balance and trades

--- src/controllers/accounts.ts
+++ src/controllers/accounts.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../../config";
 import { socket } from "../utils/websocket";
+import ccxt from "ccxt";
 
 export const getAccounts = async (_req: Request, res: Response) => {
     try {
@@ -79,6 +80,18 @@ export const editAccount = async (req: Request, res: Response) => {
     }
 }
 
+export const editAllAccounts = async (req: Request, res: Response) => {
+    try {
+        const updatedAccounts = await prisma.credentials.updateMany({
+            data: req.body
+        })
+        res.status(200).json(updatedAccounts);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+}
+
 export const setActiveAccount = async (req: Request, res: Response) => {
     try {
         const { id } = req.params
@@ -117,5 +130,50 @@ export const deleteAccount = async (req: Request, res: Response) => {
     }
     catch (error) {
         res.status(500).json(error);
+    }
+}
+
+export const getAccountBalance = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const account = await prisma.credentials.findUnique({ where: { id: Number(id) } });
+        if (!account) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+        const bybit = new ccxt.bybit({ apiKey: account.api, secret: account.secret });
+        const balance = await bybit.privateGetV2PrivateWalletBalance({ coin: 'USDT' });
+        const parsedBalance = {
+            total: parseFloat(balance.result.USDT.wallet_balance),
+            equity: parseFloat(balance.result.USDT.equity),
+            unrealised: parseFloat(balance.result.USDT.unrealised_pnl)
+        }
+        return res.status(200).json(parsedBalance);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+export const getAccountPositions = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const account = await prisma.credentials.findUnique({ where: { id: Number(id) } });
+        if (!account) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+        const bybit = new ccxt.bybit({ apiKey: account.api, secret: account.secret });
+        const rawPositions = await bybit.fetchPositions();
+        const filteredPos = rawPositions.filter((position: any) => position.contracts > 0);
+        const positions = filteredPos.map((pos: any) => {
+            return {
+                symbol: pos.info.symbol,
+                size: pos.info.size,
+                leverage: pos.info.leverage,
+                value: pos.info.position_value,
+                side: pos.info.side
+            }
+        });
+        return res.status(200).json(positions);
+    } catch (error) {
+        return res.status(500).json(error);
     }
 }
--- src/routes/accounts.routes.ts
+++ src/routes/accounts.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createAccount, deleteAccount, getAccount, getAccounts, setActiveAccount, editAccount } from "../controllers/accounts";
+import { createAccount, deleteAccount, getAccount, getAccounts, setActiveAccount, editAccount, editAllAccounts, getAccountBalance, getAccountPositions } from "../controllers/accounts";
 
 const router = Router();
 
 router.get('/', getAccounts);
 router.get('/:id', getAccount);
+router.get('/:id/balance', getAccountBalance);
+router.get('/:id/positions', getAccountPositions);
 router.post('/', createAccount);
+router.put('/', editAllAccounts);
 router.put('/:id', editAccount);
 router.put('/:id/active', setActiveAccount);
 router.delete('/:id', deleteAccount);
